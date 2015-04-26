4a36cd65ed1e4899c8b14d099f30d727e4fbfeaf
Brianvoe <brian@webiswhatido.com>
2015-04-26 15:11:46 -0500

    unique id - added example and benchmark

--- unique_test.go
+++ unique_test.go
@@ -1,11 +1,26 @@
 package gofakeit
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestUniqueId(t *testing.T) {
 	id := UniqueId()
 
 	if len(id) != 32 {
 		t.Error("unique length does not equal requested length")
+	}
+}
+
+func ExampleUniqueId() {
+	Seed(11)
+	fmt.Println(UniqueId())
+	// Output: Q3LewXJfQuPrnLiduMnTnQcsGJ1NqBmv
+}
+
+func BenchmarkUniqueId(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		UniqueId()
 	}
 }
