4ca853de17a5c5d1513e2f1dc031a1b545d7143f
Brianvoe <brian@webiswhatido.com>
2015-04-26 19:56:49 -0500

    added ip addresses benchmarks and examples.

--- internet.go
+++ internet.go
@@ -1,6 +1,22 @@
 package gofakeit
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 // Generate Domain Suffix
 func DomainSuffix() string {
 	return getRandValue([]string{"internet", "domain_suffix"})
+}
+
+// Generate
+func IPv4Address() string {
+	num := func() int { return 2 + rand.Intn(254) }
+	return fmt.Sprintf("%d.%d.%d.%d", num(), num(), num(), num())
+}
+
+func IPv6Address() string {
+	num := 65536
+	return fmt.Sprintf("2001:cafe:%x:%x:%x:%x:%x:%x", rand.Intn(num), rand.Intn(num), rand.Intn(num), rand.Intn(num), rand.Intn(num), rand.Intn(num))
 }
--- internet_test.go
+++ internet_test.go
@@ -16,3 +16,27 @@ func BenchmarkDomainSuffix(b *testing.B) {
 		DomainSuffix()
 	}
 }
+
+func ExampleIPv4Address() {
+	Seed(11)
+	fmt.Println(IPv4Address())
+	// Output: 222.83.191.222
+}
+
+func BenchmarkIPv4Address(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		IPv4Address()
+	}
+}
+
+func ExampleIPv6Address() {
+	Seed(11)
+	fmt.Println(IPv6Address())
+	// Output: 2001:cafe:8898:ee17:bc35:9064:5866:d019
+}
+
+func BenchmarkIPv6Address(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		IPv6Address()
+	}
+}
