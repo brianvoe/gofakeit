5262de97dfd2557e6995f3c6c53ce21c175deb4c
Brianvoe <brian@webiswhatido.com>
2015-04-26 22:07:30 -0500

    file - updated mime types and added extensions.

--- file.go
+++ file.go
@@ -3,3 +3,7 @@ package gofakeit
 func MimeType() string {
 	return getRandValue([]string{"file", "mime_type"})
 }
+
+func Extension() string {
+	return getRandValue([]string{"file", "extension"})
+}
--- file_test.go
+++ file_test.go
@@ -8,11 +8,23 @@ import (
 func ExampleMimeType() {
 	Seed(11)
 	fmt.Println(MimeType())
-	// Output: video/3gpp2
+	// Output: application/dsptype
 }
 
 func BenchmarkMimeType(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		MimeType()
+	}
+}
+
+func ExampleExtension() {
+	Seed(11)
+	fmt.Println(Extension())
+	// Output: nes
+}
+
+func BenchmarkExtension(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Extension()
 	}
 }
