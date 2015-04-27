4ef09a37b860177af79bb79a884899704bdea3df
Brianvoe <brian@webiswhatido.com>
2015-04-26 22:08:12 -0500

    file - updated comments.

--- file.go
+++ file.go
@@ -1,9 +1,11 @@
 package gofakeit
 
+// Generate Random File Mime Type
 func MimeType() string {
 	return getRandValue([]string{"file", "mime_type"})
 }
 
+// Generate Random File Extension
 func Extension() string {
 	return getRandValue([]string{"file", "extension"})
 }
