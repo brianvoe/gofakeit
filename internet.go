8289fe4fee1500c6147f698d958c08c799253927
Brianvoe <brian@webiswhatido.com>
2015-05-02 22:27:53 -0500

    internet - added username and moved password

--- internet.go
+++ internet.go
@@ -10,13 +10,57 @@ func DomainSuffix() string {
 	return getRandValue([]string{"internet", "domain_suffix"})
 }
 
-// Generate
+// Generate IP Version 4
 func IPv4Address() string {
 	num := func() int { return 2 + rand.Intn(254) }
 	return fmt.Sprintf("%d.%d.%d.%d", num(), num(), num(), num())
 }
 
+// Generate IP Version 6
 func IPv6Address() string {
 	num := 65536
 	return fmt.Sprintf("2001:cafe:%x:%x:%x:%x:%x:%x", rand.Intn(num), rand.Intn(num), rand.Intn(num), rand.Intn(num), rand.Intn(num), rand.Intn(num))
+}
+
+// Generate Username
+func Username() string {
+	return getRandValue([]string{"name", "last"}) + replaceWithNumbers("####")
+}
+
+// Generator Password
+func Password(lower bool, upper bool, numeric bool, special bool, space bool, length int) string {
+	var passString string = ""
+	var lowerStr string = "abcdefghijklmnopqrstuvwxyz"
+	var upperStr string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	var numericStr string = "0123456789"
+	var specialStr string = "!@#$%&?-_"
+	var spaceStr string = " "
+
+	if lower {
+		passString += lowerStr
+	}
+	if upper {
+		passString += upperStr
+	}
+	if numeric {
+		passString += numericStr
+	}
+	if special {
+		passString += specialStr
+	}
+	if space {
+		passString += spaceStr
+	}
+
+	// Set default if empty
+	if passString == "" {
+		passString = lowerStr + numericStr
+	}
+
+	passBytes := []byte(passString)
+	finalBytes := make([]byte, length)
+	for i := 0; i < length; i++ {
+		finalBytes[i] = passBytes[randIntRange(0, len(passBytes))]
+	}
+	return string(finalBytes)
 }
