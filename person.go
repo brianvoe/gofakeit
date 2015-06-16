596c47f7d4eb6182f4513f5f657257e8eed775ae
Brian Voelker <brian@webiswhatido.com>
2015-06-16 12:14:06 -0500

    Person - added gender and person struct full of a bunch of useful information.

--- person.go
+++ person.go
@@ -4,3 +4,38 @@ package gofakeit
 func SSN() string {
 	return Numerify("###-###-####")
 }
+
+func Gender() string {
+	if Bool() == true {
+		return "male"
+	}
+
+	return "female"
+}
+
+type PersonInfo struct {
+	FirstName  string
+	LastName   string
+	Gender     string
+	SSN        string
+	Image      string
+	Job        *JobInfo
+	Address    *AddressInfo
+	Contact    *ContactInfo
+	CreditCard *CreditCardInfo
+}
+
+// Generate struct full of person information
+func Person() *PersonInfo {
+	return &PersonInfo{
+		FirstName:  FirstName(),
+		LastName:   LastName(),
+		Gender:     Gender(),
+		SSN:        SSN(),
+		Image:      ImageUrl(300, 300) + "/people",
+		Job:        Job(),
+		Address:    Address(),
+		Contact:    Contact(),
+		CreditCard: CreditCard(),
+	}
+}
--- person_test.go
+++ person_test.go
@@ -16,3 +16,82 @@ func BenchmarkSSN(b *testing.B) {
 		SSN()
 	}
 }
+
+func ExampleGender() {
+	Seed(11)
+	fmt.Println(Gender())
+	// Output: female
+}
+
+func BenchmarkGender(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Gender()
+	}
+}
+
+func ExamplePerson() {
+	Seed(11)
+	person := Person()
+	job := person.Job
+	address := person.Address
+	contact := person.Contact
+	creditCard := person.CreditCard
+
+	fmt.Println(person.FirstName)
+	fmt.Println(person.LastName)
+	fmt.Println(person.Gender)
+	fmt.Println(person.SSN)
+	fmt.Println(person.Image)
+
+	fmt.Println(job.Company)
+	fmt.Println(job.Title)
+	fmt.Println(job.Descriptor)
+	fmt.Println(job.Level)
+
+	fmt.Println(address.Address)
+	fmt.Println(address.Street)
+	fmt.Println(address.City)
+	fmt.Println(address.State)
+	fmt.Println(address.Zip)
+	fmt.Println(address.Country)
+	fmt.Println(address.Latitude)
+	fmt.Println(address.Longitude)
+
+	fmt.Println(contact.Phone)
+	fmt.Println(contact.Email)
+
+	fmt.Println(creditCard.Type)
+	fmt.Println(creditCard.Number)
+	fmt.Println(creditCard.Exp)
+	fmt.Println(creditCard.Cvv)
+
+	// Output: Markus
+	// Moen
+	// male
+	// 727-157-0245
+	// http://lorempixel.com/300/300/people
+	// Jacobi-Kuhic
+	// Director
+	// Forward
+	// Accounts
+	// 776 Lake View land, Rueckerstad, New Hampshire 82250-2868
+	// 776 Lake View land
+	// Rueckerstad
+	// New Hampshire
+	// 82250-2868
+	// New Caledonia
+	// -77.27269007503322
+	// 44.31630890565333
+	// 1-336-154-4071
+	// margaritarath@labadie.com
+	// Discover
+	// 6548275542463278
+	// 08/18
+	// 841
+}
+
+func BenchmarkPerson(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Person()
+	}
+}
