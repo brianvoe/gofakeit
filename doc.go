b01b8c7cc9cb21f9ac5260640e54b9aa55a724e5
Brianvoe <brian@webiswhatido.com>
2017-06-12 21:14:02 -0500

    docs - minor updates

--- doc.go
+++ doc.go
@@ -1,8 +1,24 @@
 /*
-Package gofakeit is an extensive fake data generator.
+Package gofakeit is a random data generator written in go
 
-A large goal is to keep things extremely simple, clean, self contained, tested and documented.
+Every function has an example and a benchmark
 
-Contributions are welcome as im sure things can always be better.
+See the full list here https://godoc.org/github.com/brianvoe/gofakeit
+
+Examples:
+  import "github.com/brianvoe/gofakeit"
+
+  gofakeit.Name() // Markus Moen
+  gofakeit.Email() // alaynawuckert@kozey.biz
+  gofakeit.Phone() // (570)245-7485
+  gofakeit.BS() // front-end
+  gofakeit.BeerName() // Duvel
+  gofakeit.Color() // MediumOrchid
+  gofakeit.Company() // Moen, Pagac and Wuckert
+  gofakeit.CreditCardNumber() // 4287271570245748
+  gofakeit.HackerPhrase() // Connecting the array won't do anything, we need to generate the haptic COM driver!
+  gofakeit.JobTitle() // Director
+  gofakeit.Password(true, true, true, true, true, 32) // WV10MzLxq2DX79w1omH97_0ga59j8!kj
+  // 80+ more!!!
 */
 package gofakeit
