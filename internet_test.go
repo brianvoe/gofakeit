1d36ef70235162b6b2c0257e577bf4d4d6b8f9ac
Harry Bagdi <hbagdi1@binghamton.edu>
2018-01-02 22:08:31 -0800

    fix: Test cases

--- internet_test.go
+++ internet_test.go
@@ -8,7 +8,7 @@ import (
 func ExampleDomainName() {
 	Seed(11)
 	fmt.Println(DomainName())
-	// Output: centraltarget.org
+	// Output: centraltarget.biz
 }
 
 func BenchmarkDomainName(b *testing.B) {
