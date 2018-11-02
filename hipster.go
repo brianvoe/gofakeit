6f5baeaf88ff9e46b5e5cc39795625b190880d29
B.G.Adrian <aditza8@gmail.com>
2018-11-02 17:56:42 +0200

    refactor sentence/paragraph after the hipster; removed duplicate code;

--- hipster.go
+++ hipster.go
@@ -1,10 +1,5 @@
 package gofakeit
 
-import (
-	"bytes"
-	"unicode"
-)
-
 // HipsterWord will return a single hipster word
 func HipsterWord() string {
 	return getRandValue([]string{"hipster", "word"})
@@ -12,61 +7,14 @@ func HipsterWord() string {
 
 // HipsterSentence will generate a random sentence
 func HipsterSentence(wordCount int) string {
-	if wordCount <= 0 {
-		return ""
-	}
-
-	wordSeparator := ' '
-	sentence := bytes.Buffer{}
-	//we try to guess the size
-	sentence.Grow(wordCount * 6)
-
-	for i := 0; i < wordCount; i++ {
-		word := getRandValue([]string{"hipster", "word"})
-		if i == 0 { //First letter should be capital
-			runes := []rune(word)
-			runes[0] = unicode.ToTitle(runes[0])
-			word = string(runes)
-		}
-		sentence.WriteString(word)
-
-		if i < wordCount-1 {
-			sentence.WriteRune(wordSeparator)
-		}
-	}
-
-	sentence.WriteRune('.')
-	return sentence.String()
+	return sentence(wordCount, HipsterWord)
 }
 
-// HipsterParagraph will generate a random paragraph
+// HipsterParagraph will generate a random paragraphGenerator
 // Set Paragraph Count
 // Set Sentence Count
 // Set Word Count
 // Set Paragraph Separator
 func HipsterParagraph(paragraphCount int, sentenceCount int, wordCount int, separator string) string {
-	if paragraphCount <= 0 || sentenceCount <= 0 || wordCount <= 0 {
-		return ""
-	}
-
-	//to avoid making Go 1.10 dependency, we cannot use strings.Builder
-	paragraphs := bytes.Buffer{}
-	//we presume the length
-	paragraphs.Grow(paragraphCount * sentenceCount * wordCount * 6)
-	wordSeparator := ' '
-
-	for i := 0; i < paragraphCount; i++ {
-		for e := 0; e < sentenceCount; e++ {
-			paragraphs.WriteString(HipsterSentence(wordCount))
-			if e < sentenceCount-1 {
-				paragraphs.WriteRune(wordSeparator)
-			}
-		}
-
-		if i < paragraphCount-1 {
-			paragraphs.WriteString(separator)
-		}
-	}
-
-	return paragraphs.String()
+	return paragraphGenerator(paragraphCount, sentenceCount, wordCount, separator, HipsterSentence)
 }
--- words.go
+++ words.go
@@ -1,6 +1,14 @@
 package gofakeit
 
-import "strings"
+import (
+	"bytes"
+	"unicode"
+)
+
+const bytesPerWordEstimation = 6
+
+type sentenceGenerator func(wordCount int) string
+type wordGenerator func() string
 
 // Word will generate a random word
 func Word() string {
@@ -9,31 +17,66 @@ func Word() string {
 
 // Sentence will generate a random sentence
 func Sentence(wordCount int) string {
-	words := []string{}
-	var sentence string
-	for i := 0; i < wordCount; i++ {
-		words = append(words, getRandValue([]string{"lorem", "word"}))
-	}
-
-	sentence = strings.Join(words, " ")
-	return strings.ToUpper(sentence[:1]) + sentence[1:] + "."
+	return sentence(wordCount, Word)
 }
 
-// Paragraph will generate a random paragraph
+// Paragraph will generate a random paragraphGenerator
 // Set Paragraph Count
 // Set Sentence Count
 // Set Word Count
 // Set Paragraph Separator
 func Paragraph(paragraphCount int, sentenceCount int, wordCount int, separator string) string {
-	var sentences []string
-	paragraphs := []string{}
-	for i := 0; i < paragraphCount; i++ {
-		sentences = []string{}
-		for e := 0; e < sentenceCount; e++ {
-			sentences = append(sentences, Sentence(wordCount))
-		}
-		paragraphs = append(paragraphs, strings.Join(sentences, " "))
+	return paragraphGenerator(paragraphCount, sentenceCount, wordCount, separator, Sentence)
+}
+
+func sentence(wordCount int, word wordGenerator) string {
+	if wordCount <= 0 {
+		return ""
 	}
 
-	return strings.Join(paragraphs, separator)
+	wordSeparator := ' '
+	sentence := bytes.Buffer{}
+	sentence.Grow(wordCount * bytesPerWordEstimation)
+
+	for i := 0; i < wordCount; i++ {
+		word := word()
+		if i == 0 {
+			runes := []rune(word)
+			runes[0] = unicode.ToTitle(runes[0])
+			word = string(runes)
+		}
+		sentence.WriteString(word)
+		if i < wordCount-1 {
+			sentence.WriteRune(wordSeparator)
+		}
+	}
+	sentence.WriteRune('.')
+	return sentence.String()
+}
+
+func paragraphGenerator(paragraphCount int, sentenceCount int, wordCount int, separator string, sentecer sentenceGenerator) string {
+	if paragraphCount <= 0 || sentenceCount <= 0 || wordCount <= 0 {
+		return ""
+	}
+
+	//to avoid making Go 1.10 dependency, we cannot use strings.Builder
+	paragraphs := bytes.Buffer{}
+	//we presume the length
+	paragraphs.Grow(paragraphCount * sentenceCount * wordCount * bytesPerWordEstimation)
+	wordSeparator := ' '
+
+	for i := 0; i < paragraphCount; i++ {
+		for e := 0; e < sentenceCount; e++ {
+			paragraphs.WriteString(sentecer(wordCount))
+			if e < sentenceCount-1 {
+				paragraphs.WriteRune(wordSeparator)
+			}
+		}
+
+		if i < paragraphCount-1 {
+			paragraphs.WriteString(separator)
+		}
+	}
+
+	return paragraphs.String()
 }
