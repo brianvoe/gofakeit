ad2b73ba3e7c682503feec0a189e06cddc321516
Adam Babik <a.babik@designfortress.com>
2020-04-02 10:55:31 +0200

    add unit tests

--- cmd/gofakeit/internal/funcs.go
+++ cmd/gofakeit/internal/funcs.go
@@ -8,10 +8,12 @@ import (
 	"strings"
 )
 
+type funcDecl = ast.FuncDecl
+
 // ExportedFuncs returns exported functions from the path which must be a directory.
 // Functions, additionally to being exported, must return a string.
 // TODO: add support for functions taking one or more params.
-func ExportedFuncs(path string) ([]*ast.FuncDecl, error) {
+func ExportedFuncs(path string) ([]*funcDecl, error) {
 	fs := token.NewFileSet()
 	pkgs, err := parser.ParseDir(fs, path, func(info os.FileInfo) bool {
 		return !strings.HasSuffix(info.Name(), "_test.go")
@@ -19,7 +21,7 @@ func ExportedFuncs(path string) ([]*ast.FuncDecl, error) {
 	if err != nil {
 		return nil, err
 	}
-	var funcs []*ast.FuncDecl
+	var funcs []*funcDecl
 	for _, pkg := range pkgs {
 		for _, file := range pkg.Files {
 			for _, decl := range file.Decls {
@@ -36,7 +38,7 @@ func ExportedFuncs(path string) ([]*ast.FuncDecl, error) {
 
 // isValidFunc validates whether a function is exported,
 // takes no parameters and returns a single string value.
-func isValidFunc(fn *ast.FuncDecl) bool {
+func isValidFunc(fn *funcDecl) bool {
 	// Only exporter functions can be executed.
 	if !fn.Name.IsExported() {
 		return false
@@ -45,14 +47,18 @@ func isValidFunc(fn *ast.FuncDecl) bool {
 	if fn.Type.Params != nil && fn.Type.Params.NumFields() > 0 {
 		return false
 	}
+	// Accept only functions that return a single string value.
+	return funcReturnsString(fn)
+}
+
+// funcReturnsString returns true if a function returns
+// a single string value as a result.
+func funcReturnsString(fn *funcDecl) bool {
 	// Accept only functions that return a single result.
 	if fn.Type.Results == nil || fn.Type.Results.NumFields() != 1 {
 		return false
 	}
 	// The returned result must be a string.
 	ident, ok := fn.Type.Results.List[0].Type.(*ast.Ident)
-	if !ok || ident.Name != "string" {
-		return false
-	}
-	return true
+	return ok && ident.Name == "string"
 }
