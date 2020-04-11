8518860d23233e5fe8d26393ab0d752a0481bcb7
brianvoe <brian@webiswhatido.com>
2020-04-10 22:30:57 -0500

    image - added image, jpeg and png generator

--- image.go
+++ image.go
@@ -1,7 +1,12 @@
 package gofakeit
 
 import (
+	"bytes"
 	"errors"
+	"image"
+	"image/color"
+	"image/jpeg"
+	"image/png"
 	"strconv"
 )
 
@@ -10,14 +15,45 @@ func ImageURL(width int, height int) string {
 	return "https://picsum.photos/" + strconv.Itoa(width) + "/" + strconv.Itoa(height)
 }
 
+// Image generates a random rgba image
+func Image(width int, height int) *image.RGBA {
+	upLeft := image.Point{0, 0}
+	lowRight := image.Point{width, height}
+
+	img := image.NewRGBA(image.Rectangle{upLeft, lowRight})
+
+	// Set color for each pixel
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			img.Set(x, y, color.RGBA{uint8(Number(0, 255)), uint8(Number(0, 255)), uint8(Number(0, 255)), 0xff})
+		}
+	}
+
+	return img
+}
+
+// ImageJpeg generates a random rgba jpeg image
+func ImageJpeg(width int, height int) []byte {
+	buf := new(bytes.Buffer)
+	jpeg.Encode(buf, Image(width, height), nil)
+	return buf.Bytes()
+}
+
+// ImagePng generates a random rgba png image
+func ImagePng(width int, height int) []byte {
+	buf := new(bytes.Buffer)
+	png.Encode(buf, Image(width, height))
+	return buf.Bytes()
+}
+
 func addImageLookup() {
 	AddLookupData("imageurl", Info{
 		Category:    "image",
 		Description: "Random image url",
 		Example:     "https://picsum.photos/500/500",
 		Params: []Param{
-			{Field: "width", Required: false, Type: "int", Default: "500", Description: "Image width"},
-			{Field: "height", Required: false, Type: "int", Default: "500", Description: "Image height"},
+			{Field: "width", Type: "int", Default: "500", Description: "Image width"},
+			{Field: "height", Type: "int", Default: "500", Description: "Image height"},
 		},
 		Call: func(m *map[string][]string, info *Info) (interface{}, error) {
 			width, err := info.GetInt(m, "width")
@@ -37,6 +73,64 @@ func addImageLookup() {
 			}
 
 			return ImageURL(width, height), nil
+		},
+	})
+
+	AddLookupData("imagejpeg", Info{
+		Category:    "image",
+		Description: "Random jpeg image",
+		Example:     "file.jpeg - bytes",
+		Params: []Param{
+			{Field: "width", Type: "int", Default: "500", Description: "Image width"},
+			{Field: "height", Type: "int", Default: "500", Description: "Image height"},
+		},
+		Call: func(m *map[string][]string, info *Info) (interface{}, error) {
+			width, err := info.GetInt(m, "width")
+			if err != nil {
+				return nil, err
+			}
+			if width < 10 || width >= 1000 {
+				return nil, errors.New("Invalid image width, must be greater than 10, less than 1000")
+			}
+
+			height, err := info.GetInt(m, "height")
+			if err != nil {
+				return nil, err
+			}
+			if height < 10 || height >= 1000 {
+				return nil, errors.New("Invalid image height, must be greater than 10, less than 1000")
+			}
+
+			return ImageJpeg(width, height), nil
+		},
+	})
+
+	AddLookupData("imagepng", Info{
+		Category:    "image",
+		Description: "Random png image",
+		Example:     "file.png - bytes",
+		Params: []Param{
+			{Field: "width", Type: "int", Default: "500", Description: "Image width"},
+			{Field: "height", Type: "int", Default: "500", Description: "Image height"},
+		},
+		Call: func(m *map[string][]string, info *Info) (interface{}, error) {
+			width, err := info.GetInt(m, "width")
+			if err != nil {
+				return nil, err
+			}
+			if width < 10 || width >= 1000 {
+				return nil, errors.New("Invalid image width, must be greater than 10, less than 1000")
+			}
+
+			height, err := info.GetInt(m, "height")
+			if err != nil {
+				return nil, err
+			}
+			if height < 10 || height >= 1000 {
+				return nil, errors.New("Invalid image height, must be greater than 10, less than 1000")
+			}
+
+			return ImagePng(width, height), nil
 		},
 	})
 }
