d36ffde06d9b7b2757f6fb846499ff8431fe6cf3
brianvoe <brian@webiswhatido.com>
2020-04-28 22:15:51 -0500

    generate - added regex and removed json(going to make a more diverse one)

--- generate_test.go
+++ generate_test.go
@@ -2,6 +2,7 @@ package gofakeit
 
 import (
 	"fmt"
+	"regexp"
 	"testing"
 )
 
@@ -31,6 +32,67 @@ func BenchmarkGenerate(b *testing.B) {
 	}
 }
 
+func ExampleRegex() {
+	Seed(11)
+
+	fmt.Println(Regex("[abcdef]{5}"))
+	fmt.Println(Regex("[[:upper:]]{5}"))
+	fmt.Println(Regex("(hello|world|whats|up)"))
+	fmt.Println(Regex(`^[a-z]{5,10}@[a-z]{5,10}\.(com|net|org)$`))
+	// Output: affec
+	// RXHKI
+	// up
+	// ptapwy@dnsmkgtl.com
+}
+
+var regexes = []struct{ test string }{
+	{`^\d+$`},
+	{`\D{3}`},
+	{`Z{2,5}`},
+	{`[^1]{3,5}`},
+	{`(ab|bc)def`},
+	{`((123)?){3}`},
+	{`[^abcdef]{5}`},
+	{`[a-zA-Z]{100}`},
+	{`[[:upper:]]{5}`},
+	{`[^0-5a-z\s]{5}`},
+	{`123[0-2]+.*\w{3}`},
+	{`(hello|world|whats|up)`},
+	{`^\d{1,2}[/](1[0-2]|[1-9])[/]((19|20)\d{2})$`},
+	{`^((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])$`},
+	{"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"},
+}
+
+func TestRegex(t *testing.T) {
+	for _, r := range regexes {
+		for i := 0; i < 100; i++ {
+			// Try to compile regexTest
+			regCompile, err := regexp.Compile(r.test)
+			if err != nil {
+				t.Fatal("Error compiling. regex: ", r.test, " failed to compile:", err)
+			}
+
+			// Generate string and test it matches the regex syntax
+			reg := Regex(r.test)
+			if !regCompile.MatchString(reg) {
+				t.Error("Generated data does not match regex. Regex: ", r.test, " output: ", reg)
+			}
+		}
+	}
+}
+
+func BenchmarkRegex(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Regex(`(hello|world|whats|up)`)
+	}
+}
+
+func BenchmarkRegexEmail(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Regex("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+	}
+}
+
 func ExampleMap() {
 	Seed(11)
 
@@ -47,45 +109,5 @@ func TestMap(t *testing.T) {
 func BenchmarkMap(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Map()
-	}
-}
-
-func ExampleJSON() {
-	Seed(11)
-
-	fmt.Println(JSON(false))
-	fmt.Println(JSON(true))
-	// Output:
-	// 	{"approach":{"mind":["arrival","should","resolve","outcome","hurt"]},"arrive":"Coordinator","consult":"respond","context":9285735,"water":5081652}
-	// {
-	//     "document": {
-	//         "heat": "empower"
-	//     },
-	//     "hair": {
-	//         "arrange": 191860.03
-	//     },
-	//     "intelligence": 628419.75,
-	//     "manchester": {
-	//         "agreement": 1303185
-	//     },
-	//     "provide": [
-	//         "tom",
-	//         "pay",
-	//         "mark",
-	//         "teacher",
-	//         "slip"
-	//     ],
-	//     "rugby": {
-	//         "own": "Macie Cartwright"
-	//     },
-	//     "satisfy": {
-	//         "predict": 162934
-	//     }
-	// }
-}
-
-func BenchmarkJSON(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		JSON(Bool())
 	}
 }
