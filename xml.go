25067943a47ff56e6240be4ff89b93e6492717f1
brianvoe <brian@webiswhatido.com>
2020-05-13 00:13:58 -0500

    xml - work in progress

--- xml.go
+++ xml.go
@@ -4,99 +4,123 @@ import (
 	"bytes"
 	"encoding/xml"
 	"errors"
+	"reflect"
 )
 
 // XMLOptions defines values needed for json generation
 type XMLOptions struct {
-	Type          string  `json:"type"` // single or multiple
-	RootElement   string  `json:"root_element"`
-	RecordElement string  `json:"record_element"`
-	RowCount      int     `json:"row_count"`
-	Fields        []Field `json:"fields"`
-	Indent        bool    `json:"indent"`
+	Type          string  `json:"type" xml:"type"` // single or multiple
+	RootElement   string  `json:"root_element" xml:"root_element"`
+	RecordElement string  `json:"record_element" xml:"record_element"`
+	RowCount      int     `json:"row_count" xml:"row_count"`
+	Fields        []Field `json:"fields" xml:"fields"`
+	Indent        bool    `json:"indent" xml:"indent"`
 }
 
-type xmlMap map[string]interface{}
+type xmlArray struct {
+	XMLName xml.Name
+	Array   []xmlMap
+}
 
-type xmlMapEntry struct {
+type xmlMap struct {
+	XMLName xml.Name
+	Map     map[string]interface{} `xml:",chardata"`
+}
+
+type xmlEntry struct {
 	XMLName xml.Name
 	Value   interface{} `xml:",chardata"`
 }
 
 func (m xmlMap) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
-	if len(m) == 0 {
+	if len(m.Map) == 0 {
 		return nil
 	}
+
+	start.Name = m.XMLName
 
 	err := e.EncodeToken(start)
 	if err != nil {
 		return err
 	}
 
-	for key, value := range m {
-		// fmt.Println(fmt.Sprintf("%T", value))
-		// x, err := xml.MarshalIndent(value, "", "  ")
-		// if err != nil {
-		// 	return err
-		// }
-		e.Encode(xmlMapEntry{XMLName: xml.Name{Local: key}, Value: value})
+	err = xmlMapLoop(e, &m)
+	if err != nil {
+		return err
 	}
 
 	return e.EncodeToken(start.End())
+}
 
-	// tokens := []xml.Token{start}
+func xmlMapLoop(e *xml.Encoder, m *xmlMap) error {
+	var err error
+	for key, value := range m.Map {
+		v := reflect.ValueOf(value)
+		switch v.Kind() {
+		case reflect.Bool,
+			reflect.String,
+			reflect.Int, reflect.Int8, reflect.Int32, reflect.Int64,
+			reflect.Uint, reflect.Uint8, reflect.Uint32, reflect.Uint64,
+			reflect.Float32, reflect.Float64:
+			err = e.Encode(xmlEntry{XMLName: xml.Name{Local: key}, Value: value})
+			if err != nil {
+				return err
+			}
+		case reflect.Slice:
+			e.EncodeToken(xml.StartElement{Name: xml.Name{Local: key}})
+			for i := 0; i < v.Len(); i++ {
+				err = e.Encode(xmlEntry{XMLName: xml.Name{Local: "value"}, Value: v.Index(i).String()})
+				if err != nil {
+					return err
+				}
+			}
+			e.EncodeToken(xml.EndElement{Name: xml.Name{Local: key}})
+		case reflect.Map:
+			err = e.Encode(xmlMap{
+				XMLName: xml.Name{Local: key},
+				Map:     value.(map[string]interface{}),
+			})
+			if err != nil {
+				return err
+			}
+		default:
+			err = e.Encode(value)
+			if err != nil {
+				return err
+			}
+		}
+	}
 
-	// for key, value := range m {
-	// 	// fmt.Println(fmt.Sprintf("%T", value))
-	// 	// x, err := xml.MarshalIndent(value, "", "    ")
-	// 	// if err != nil {
-	// 	// 	return err
-	// 	// }
-
-	// 	var b bytes.Buffer
-	// 	x := xml.NewEncoder(&b)
-	// 	// if jo.Indent {
-	// 	// 	x.Indent("", "    ")
-	// 	// }
-	// 	err := x.Encode(value)
-	// 	if err != nil {
-	// 		return err
-	// 	}
-
-	// 	t := xml.StartElement{Name: xml.Name{Space: "", Local: key}}
-	// 	tokens = append(tokens, t, b, xml.EndElement{Name: t.Name})
-	// }
-
-	// tokens = append(tokens, xml.EndElement{Name: start.Name})
-
-	// for _, t := range tokens {
-	// 	err := e.EncodeToken(t)
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// }
-
-	// // flush to ensure tokens are written
-	// return e.Flush()
+	return nil
 }
 
 // XML generates an object or an array of objects in json format
-func XML(jo *XMLOptions) ([]byte, error) {
+func XML(xo *XMLOptions) ([]byte, error) {
 	// Check to make sure they passed in a type
-	if jo.Type != "multiple" && jo.Type != "single" {
+	if xo.Type != "single" && xo.Type != "array" {
 		return nil, errors.New("Invalid type, must be array or object")
 	}
 
-	if jo.Fields == nil || len(jo.Fields) <= 0 {
+	if xo.Fields == nil || len(xo.Fields) <= 0 {
 		return nil, errors.New("Must pass fields in order to build json object(s)")
 	}
 
-	if jo.Type == "single" {
-		v := xmlMap{}
-		// v.XMLName = xml.Name{Local: jo.RecordElement}
+	if xo.RootElement == "" {
+		xo.RecordElement = "xml"
+	}
+
+	if xo.RecordElement == "" {
+		xo.RecordElement = "record"
+	}
+
+	if xo.Type == "single" {
+		v := xmlMap{
+			XMLName: xml.Name{Local: xo.RootElement},
+			Map:     make(map[string]interface{}),
+		}
 
 		// Loop through fields and add to them to map[string]interface{}
-		for _, field := range jo.Fields {
+		for _, field := range xo.Fields {
 			// Get function info
 			funcInfo := GetFuncLookup(field.Function)
 			if funcInfo == nil {
@@ -108,13 +132,13 @@ func XML(jo *XMLOptions) ([]byte, error) {
 				return nil, err
 			}
 
-			v[field.Name] = value
+			v.Map[field.Name] = value
 		}
 
 		// Marshal into bytes
 		var b bytes.Buffer
 		x := xml.NewEncoder(&b)
-		if jo.Indent {
+		if xo.Indent {
 			x.Indent("", "    ")
 		}
 		err := x.Encode(v)
@@ -125,50 +149,60 @@ func XML(jo *XMLOptions) ([]byte, error) {
 		return b.Bytes(), nil
 	}
 
-	// if jo.Type == "array" {
-	// 	// Make sure you set a row count
-	// 	if jo.RowCount <= 0 {
-	// 		return nil, errors.New("Must have row count")
-	// 	}
+	if xo.Type == "array" {
+		// Make sure you set a row count
+		if xo.RowCount <= 0 {
+			return nil, errors.New("Must have row count")
+		}
 
-	// 	v := []map[string]interface{}{}
+		xa := xmlArray{
+			XMLName: xml.Name{Local: xo.RootElement},
+			Array:   make([]xmlMap, xo.RowCount),
+		}
 
-	// 	for i := 1; i <= int(jo.RowCount); i++ {
-	// 		vr := map[string]interface{}{}
+		for i := 1; i <= int(xo.RowCount); i++ {
+			v := xmlMap{
+				XMLName: xml.Name{Local: xo.RecordElement},
+				Map:     make(map[string]interface{}),
+			}
 
-	// 		// Loop through fields and add to them to map[string]interface{}
-	// 		for _, field := range jo.Fields {
-	// 			if field.Function == "autoincrement" {
-	// 				vr[field.Name] = i
-	// 				continue
-	// 			}
+			// Loop through fields and add to them to map[string]interface{}
+			for _, field := range xo.Fields {
+				if field.Function == "autoincrement" {
+					v.Map[field.Name] = i
+					continue
+				}
 
-	// 			// Get function info
-	// 			funcInfo := GetFuncLookup(field.Function)
-	// 			if funcInfo == nil {
-	// 				return nil, errors.New("Invalid function, " + field.Function + " does not exist")
-	// 			}
+				// Get function info
+				funcInfo := GetFuncLookup(field.Function)
+				if funcInfo == nil {
+					return nil, errors.New("Invalid function, " + field.Function + " does not exist")
+				}
 
-	// 			value, err := funcInfo.Call(&field.Params, funcInfo)
-	// 			if err != nil {
-	// 				return nil, err
-	// 			}
+				value, err := funcInfo.Call(&field.Params, funcInfo)
+				if err != nil {
+					return nil, err
+				}
 
-	// 			vr[field.Name] = value
-	// 		}
+				v.Map[field.Name] = value
+			}
 
-	// 		v = append(v, vr)
-	// 	}
+			xa.Array = append(xa.Array, v)
+		}
 
-	// 	// Marshal into bytes
-	// 	j := []byte{}
-	// 	if jo.Indent {
-	// 		j, _ = xml.MarshalIndent(v, "", "    ")
-	// 	} else {
-	// 		j, _ = xml.Marshal(v)
-	// 	}
-	// 	return j, nil
-	// }
+		// Marshal into bytes
+		var b bytes.Buffer
+		x := xml.NewEncoder(&b)
+		if xo.Indent {
+			x.Indent("", "    ")
+		}
+		err := x.Encode(xa)
+		if err != nil {
+			return nil, err
+		}
+
+		return b.Bytes(), nil
+	}
 
 	return nil, errors.New("Invalid type, must be array or object")
 }
--- xml_test.go
+++ xml_test.go
@@ -5,13 +5,76 @@ import "fmt"
 func ExampleXML_single() {
 	Seed(11)
 
+	AddFuncLookup("randmap", Info{
+		Display:     "Rand Map",
+		Category:    "generate",
+		Description: "",
+		Example:     "",
+		Output:      "string",
+		Call: func(m *map[string][]string, info *Info) (interface{}, error) {
+			return map[string]interface{}{
+				"string": "string value",
+				"int":    123456789,
+				"float":  123.456,
+				"array":  []string{"one", "two", "three"},
+			}, nil
+		},
+	})
+
 	value, err := XML(&XMLOptions{
-		Type: "single",
+		Type:        "single",
+		RootElement: "single",
 		Fields: []Field{
 			{Name: "first_name", Function: "firstname"},
 			{Name: "last_name", Function: "lastname"},
-			{Name: "address", Function: "address"},
 			{Name: "password", Function: "password", Params: map[string][]string{"special": {"false"}}},
+			{Name: "person", Function: "person"},
+			{Name: "ints", Function: "shufflestrings", Params: map[string][]string{"strs": {"123", "456", "789"}}},
+			{Name: "rand_map", Function: "randmap"},
+		},
+		Indent: true,
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(value))
+
+	// Output: jhiuhuih
+}
+
+func ExampleXML_array() {
+	Seed(11)
+
+	AddFuncLookup("randmap", Info{
+		Display:     "Rand Map",
+		Category:    "generate",
+		Description: "",
+		Example:     "",
+		Output:      "string",
+		Call: func(m *map[string][]string, info *Info) (interface{}, error) {
+			return map[string]interface{}{
+				"string": "string value",
+				"int":    123456789,
+				"float":  123.456,
+				"array":  []string{"one", "two", "three"},
+			}, nil
+		},
+	})
+
+	value, err := XML(&XMLOptions{
+		Type:          "array",
+		RootElement:   "array",
+		RecordElement: "record",
+		RowCount:      10,
+		Fields: []Field{
+			{Name: "id", Function: "autoincrement"},
+			{Name: "first_name", Function: "firstname"},
+			{Name: "last_name", Function: "lastname"},
+			{Name: "password", Function: "password", Params: map[string][]string{"special": {"false"}}},
+			{Name: "person", Function: "person"},
+			{Name: "ints", Function: "shufflestrings", Params: map[string][]string{"strs": {"123", "456", "789"}}},
+			{Name: "rand_map", Function: "randmap"},
 		},
 		Indent: true,
 	})
