7c25fe9e6157772b1ae21c40f5557daa29f018dc
Ryan Vance <rcoy.vance@pm.me>
2021-01-02 14:24:26 -0600

    refactor beer

--- beer.go
+++ beer.go
@@ -1,45 +1,120 @@
 package gofakeit
 
-import "strconv"
+import (
+	"math/rand"
+	"strconv"
+)
 
 // BeerName will return a random beer name
 func BeerName() string {
-	return getRandValue(globalFaker.Rand, []string{"beer", "name"})
+	return beerName(globalFaker.Rand)
+}
+
+// BeerName will return a random beer name
+func (f *Faker) BeerName() string {
+	return beerName(f.Rand)
+}
+
+func beerName(r *rand.Rand) string {
+	return getRandValue(r, []string{"beer", "name"})
 }
 
 // BeerStyle will return a random beer style
 func BeerStyle() string {
-	return getRandValue(globalFaker.Rand, []string{"beer", "style"})
+	return beerStyle(globalFaker.Rand)
+}
+
+// BeerStyle will return a random beer style
+func (f *Faker) BeerStyle() string {
+	return beerStyle(f.Rand)
+}
+
+func beerStyle(r *rand.Rand) string {
+	return getRandValue(r, []string{"beer", "style"})
 }
 
 // BeerHop will return a random beer hop
 func BeerHop() string {
-	return getRandValue(globalFaker.Rand, []string{"beer", "hop"})
+	return beerHop(globalFaker.Rand)
+}
+
+// BeerHop will return a random beer hop
+func (f *Faker) BeerHop() string {
+	return beerHop(f.Rand)
+}
+
+func beerHop(r *rand.Rand) string {
+	return getRandValue(r, []string{"beer", "hop"})
 }
 
 // BeerYeast will return a random beer yeast
 func BeerYeast() string {
-	return getRandValue(globalFaker.Rand, []string{"beer", "yeast"})
+	return beerYeast(globalFaker.Rand)
+}
+
+// BeerYeast will return a random beer yeast
+func (f *Faker) BeerYeast() string {
+	return beerYeast(f.Rand)
+}
+
+func beerYeast(r *rand.Rand) string {
+	return getRandValue(r, []string{"beer", "yeast"})
 }
 
 // BeerMalt will return a random beer malt
 func BeerMalt() string {
-	return getRandValue(globalFaker.Rand, []string{"beer", "malt"})
+	return beerMalt(globalFaker.Rand)
+}
+
+// BeerMalt will return a random beer malt
+func (f *Faker) BeerMalt() string {
+	return beerMalt(f.Rand)
+}
+
+func beerMalt(r *rand.Rand) string {
+	return getRandValue(r, []string{"beer", "malt"})
 }
 
 // BeerAlcohol will return a random beer alcohol level between 2.0 and 10.0
 func BeerAlcohol() string {
-	return strconv.FormatFloat(float64Range(globalFaker.Rand, 2.0, 10.0), 'f', 1, 64) + "%"
+	return beerAlcohol(globalFaker.Rand)
+}
+
+// BeerAlcohol will return a random beer alcohol level between 2.0 and 10.0
+func (f *Faker) BeerAlcohol() string {
+	return beerAlcohol(f.Rand)
+}
+
+func beerAlcohol(r *rand.Rand) string {
+	return strconv.FormatFloat(float64Range(r, 2.0, 10.0), 'f', 1, 64) + "%"
 }
 
 // BeerIbu will return a random beer ibu value between 10 and 100
 func BeerIbu() string {
-	return strconv.Itoa(randIntRange(globalFaker.Rand, 10, 100)) + " IBU"
+	return beerIbu(globalFaker.Rand)
+}
+
+// BeerIbu will return a random beer ibu value between 10 and 100
+func (f *Faker) BeerIbu() string {
+	return beerIbu(f.Rand)
+}
+
+func beerIbu(r *rand.Rand) string {
+	return strconv.Itoa(randIntRange(r, 10, 100)) + " IBU"
 }
 
 // BeerBlg will return a random beer blg between 5.0 and 20.0
 func BeerBlg() string {
-	return strconv.FormatFloat(float64Range(globalFaker.Rand, 5.0, 20.0), 'f', 1, 64) + "°Blg"
+	return beerBlg(globalFaker.Rand)
+}
+
+// BeerBlg will return a random beer blg between 5.0 and 20.0
+func (f *Faker) BeerBlg() string {
+	return beerBlg(f.Rand)
+}
+
+func beerBlg(r *rand.Rand) string {
+	return strconv.FormatFloat(float64Range(r, 5.0, 20.0), 'f', 1, 64) + "°Blg"
 }
 
 func addBeerLookup() {
