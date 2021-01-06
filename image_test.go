f08057fb8ce28d4cb1326927dab829178115fb43
Ryan Vance <rcoy.vance@pm.me>
2021-01-05 20:56:19 -0600

    add some image tests

--- image_test.go
+++ image_test.go
@@ -11,8 +11,32 @@ func ExampleImageURL() {
 	// Output: https://picsum.photos/640/480
 }
 
+func ExampleFaker_ImageURL() {
+	f := New(11)
+	fmt.Println(f.ImageURL(640, 480))
+	// Output: https://picsum.photos/640/480
+}
+
 func BenchmarkImageURL(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		ImageURL(640, 480)
-	}
+	b.Run("package", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			ImageURL(640, 480)
+		}
+	})
+
+	b.Run("Faker math", func(b *testing.B) {
+		f := New(0)
+
+		for i := 0; i < b.N; i++ {
+			f.ImageURL(640, 480)
+		}
+	})
+
+	b.Run("Faker crypto", func(b *testing.B) {
+		f := NewCrypto()
+
+		for i := 0; i < b.N; i++ {
+			f.ImageURL(640, 480)
+		}
+	})
 }
