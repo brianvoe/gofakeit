92465c344a804bcb3fd4f34dad303f30cf415048
Ryan Vance <rcoy.vance@pm.me>
2021-01-05 20:50:46 -0600

    hacker tests

--- hacker_test.go
+++ hacker_test.go
@@ -11,10 +11,34 @@ func ExampleHackerPhrase() {
 	// Output: If we calculate the program, we can get to the AI pixel through the redundant XSS matrix!
 }
 
+func ExampleFaker_HackerPhrase() {
+	f := New(11)
+	fmt.Println(f.HackerPhrase())
+	// Output: If we calculate the program, we can get to the AI pixel through the redundant XSS matrix!
+}
+
 func BenchmarkHackerPhrase(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		HackerPhrase()
-	}
+	b.Run("package", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			HackerPhrase()
+		}
+	})
+
+	b.Run("Faker math", func(b *testing.B) {
+		f := New(0)
+
+		for i := 0; i < b.N; i++ {
+			f.HackerPhrase()
+		}
+	})
+
+	b.Run("Faker crypto", func(b *testing.B) {
+		f := NewCrypto()
+
+		for i := 0; i < b.N; i++ {
+			f.HackerPhrase()
+		}
+	})
 }
 
 func ExampleHackerAbbreviation() {
@@ -23,10 +47,34 @@ func ExampleHackerAbbreviation() {
 	// Output: ADP
 }
 
+func ExampleFaker_HackerAbbreviation() {
+	f := New(11)
+	fmt.Println(f.HackerAbbreviation())
+	// Output: ADP
+}
+
 func BenchmarkHackerAbbreviation(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		HackerAbbreviation()
-	}
+	b.Run("package", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			HackerAbbreviation()
+		}
+	})
+
+	b.Run("Faker math", func(b *testing.B) {
+		f := New(0)
+
+		for i := 0; i < b.N; i++ {
+			f.HackerAbbreviation()
+		}
+	})
+
+	b.Run("Faker crypto", func(b *testing.B) {
+		f := NewCrypto()
+
+		for i := 0; i < b.N; i++ {
+			f.HackerAbbreviation()
+		}
+	})
 }
 
 func ExampleHackerAdjective() {
@@ -35,10 +83,34 @@ func ExampleHackerAdjective() {
 	// Output: wireless
 }
 
+func ExampleFaker_HackerAdjective() {
+	f := New(11)
+	fmt.Println(f.HackerAdjective())
+	// Output: wireless
+}
+
 func BenchmarkHackerAdjective(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		HackerAdjective()
-	}
+	b.Run("package", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			HackerAdjective()
+		}
+	})
+
+	b.Run("Faker math", func(b *testing.B) {
+		f := New(0)
+
+		for i := 0; i < b.N; i++ {
+			f.HackerAdjective()
+		}
+	})
+
+	b.Run("Faker crypto", func(b *testing.B) {
+		f := NewCrypto()
+
+		for i := 0; i < b.N; i++ {
+			f.HackerAdjective()
+		}
+	})
 }
 
 func ExampleHackerNoun() {
@@ -47,10 +119,34 @@ func ExampleHackerNoun() {
 	// Output: driver
 }
 
+func ExampleFaker_HackerNoun() {
+	f := New(11)
+	fmt.Println(f.HackerNoun())
+	// Output: driver
+}
+
 func BenchmarkHackerNoun(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		HackerNoun()
-	}
+	b.Run("package", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			HackerNoun()
+		}
+	})
+
+	b.Run("Faker math", func(b *testing.B) {
+		f := New(0)
+
+		for i := 0; i < b.N; i++ {
+			f.HackerNoun()
+		}
+	})
+
+	b.Run("Faker crypto", func(b *testing.B) {
+		f := NewCrypto()
+
+		for i := 0; i < b.N; i++ {
+			f.HackerNoun()
+		}
+	})
 }
 
 func ExampleHackerVerb() {
@@ -59,10 +155,34 @@ func ExampleHackerVerb() {
 	// Output: synthesize
 }
 
+func ExampleFaker_HackerVerb() {
+	f := New(11)
+	fmt.Println(f.HackerVerb())
+	// Output: synthesize
+}
+
 func BenchmarkHackerVerb(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		HackerVerb()
-	}
+	b.Run("package", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			HackerVerb()
+		}
+	})
+
+	b.Run("Faker math", func(b *testing.B) {
+		f := New(0)
+
+		for i := 0; i < b.N; i++ {
+			f.HackerVerb()
+		}
+	})
+
+	b.Run("Faker crypto", func(b *testing.B) {
+		f := NewCrypto()
+
+		for i := 0; i < b.N; i++ {
+			f.HackerVerb()
+		}
+	})
 }
 
 func ExampleHackeringVerb() {
@@ -71,8 +191,32 @@ func ExampleHackeringVerb() {
 	// Output: connecting
 }
 
+func ExampleFaker_HackeringVerb() {
+	f := New(11)
+	fmt.Println(f.HackeringVerb())
+	// Output: connecting
+}
+
 func BenchmarkHackeringVerb(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		HackeringVerb()
-	}
+	b.Run("package", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			HackeringVerb()
+		}
+	})
+
+	b.Run("Faker math", func(b *testing.B) {
+		f := New(0)
+
+		for i := 0; i < b.N; i++ {
+			f.HackeringVerb()
+		}
+	})
+
+	b.Run("Faker crypto", func(b *testing.B) {
+		f := NewCrypto()
+
+		for i := 0; i < b.N; i++ {
+			f.HackeringVerb()
+		}
+	})
 }
