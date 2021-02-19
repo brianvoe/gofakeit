675d5f4dee2835420e717d719e5e7d860629c6d4
bvoelker <bvoelker@fluidpay.com>
2021-02-19 16:53:32 -0600

    slice - added slice usage

---
+++ slice.go
@@ -0,0 +1,16 @@
+package gofakeit
+
+import (
+	rand "math/rand"
+	"reflect"
+)
+
+// Slice fills built-in types and exported fields of a struct with random data.
+func Slice(v interface{}) { sliceFunc(globalFaker.Rand, v) }
+
+// Slice fills built-in types and exported fields of a struct with random data.
+func (f *Faker) Slice(v interface{}) { sliceFunc(f.Rand, v) }
+
+func sliceFunc(ra *rand.Rand, v interface{}) {
+	r(ra, reflect.TypeOf(v), reflect.ValueOf(v), "", -1)
+}
