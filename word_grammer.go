c18ea6218fe9fd6c96d1474fc545e23d7c4e6ec5
bvoelker <bvoelker@fluidpay.com>
2021-12-27 17:39:14 -0600

    word grammer - added simple sentence

--- word_grammer.go
+++ word_grammer.go
@@ -1,5 +1,24 @@
 package gofakeit
 
-simple, compound, complex, and compound-complex
+import (
+	rand "math/rand"
+	"unicode"
+)
 
-func sentenceSimple
+// simple, compound, complex, and compound-complex
+
+// SentenceSimple will generate a random simple sentence
+func SentenceSimple() string { return sentenceSimple(globalFaker.Rand) }
+
+// SentenceSimple will generate a random simple sentence
+func (f *Faker) SentenceSimple() string { return sentenceSimple(f.Rand) }
+
+func sentenceSimple(r *rand.Rand) string {
+	// simple sentence consists of a noun phrase and a verb phrase
+	str := phraseNoun(r) + " " + phraseVerb(r) + "."
+
+	// capitalize the first letter
+	strR := []rune(str)
+	strR[0] = unicode.ToUpper(strR[0])
+	return string(strR)
+}
